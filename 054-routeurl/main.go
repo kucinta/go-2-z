9f564ecc378ddf797987c51912a90b4826f24c89
kucinta.com <szeye@hotmail.com>
2022-03-07 00:23:42 +0800

    upfm234

--- 054-routeurl/main.go
+++ 054-routeurl/main.go
@@ -8,25 +8,23 @@ import (
 //import "fmt"
 
 func main() {
-	http.HandleFunc("/", hello)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+	//http.HandleFunc("/", hello)
 	http.HandleFunc("/about", about)
-	http.HandleFunc("/error", http.NotFound)
-	log.Println("Listening on http://0.0.0.0:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	println("This is a " + r.Method + " request")
-	//fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
-	url := r.URL.String()
-	println(url)
-	if url == "/" {
-		w.Write([]byte("Hello World"))
-	} else {
-		w.Write([]byte("404 " + url + " page not found"))
-	}
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/every-thing-else", http.NotFound)
+	log.Println("Listening on http://0.0.0.0:8054")
+	log.Fatal(http.ListenAndServe(":8054", nil))
 }
 
 func about(w http.ResponseWriter, r *http.Request) {
+	println("This is a " + r.Method + " request")
+	url := r.URL.String()
+	println(url)
 	w.Write([]byte("About me!"))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello there!"))
 }
