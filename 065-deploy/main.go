aaaa805e0755c5aca7c16922b08eb62f9bae1eda
kucinta.com <szeye@hotmail.com>
2022-03-08 01:00:31 +0800

    vscode

--- 065-deploy/main.go
+++ 065-deploy/main.go
@@ -4,14 +4,15 @@ import (
 	"065-deploy/handlers"
 	"log"
 	"net/http"
-
+	"fmt"
+	
 	"github.com/go-chi/chi"
 )
 
 func main() {
 	router := chi.NewRouter()
 	router.Get("/api/j", handlers.GetJobs)
-	//run it on port 8080
+	fmt.Println("192.168.1.234:8065")
 	err := http.ListenAndServe("0.0.0.0:8065", router)
 	if err != nil {
 		log.Fatal(err)
