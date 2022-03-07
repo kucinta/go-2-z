a15cfe0081659d5269ee6b46505fcdf2760952f4
kucinta.com <szeye@hotmail.com>
2022-03-08 06:40:01 +0800

    vsc

--- 067-uuid/main.go
+++ 067-uuid/main.go
@@ -31,7 +31,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	uuid := fmt.Sprintf("%x-%x-%x-%x-%x",
-		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	uuid := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 	fmt.Println(uuid)
 }
