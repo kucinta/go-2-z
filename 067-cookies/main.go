b069b247ef45807a51cc1969acf2a9aca0bcbdd4
kucinta.com <szeye@hotmail.com>
2022-03-08 05:17:36 +0800

    vscode

--- 067-cookies/main.go
+++ 067-cookies/main.go
@@ -66,7 +66,7 @@ func Signin(w http.ResponseWriter, r *http.Request) {
 
 	// Create a new random session token
 	// we use the "github.com/google/uuid" library to generate UUIDs
-	sessionToken := uuid.NewString()
+	ç := uuid.NewString()
 	expiresAt := time.Now().Add(120 * time.Second)
 
 	// Set the token in the session map, along with the session information
